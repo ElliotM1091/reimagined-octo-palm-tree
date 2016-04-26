3e09e2bb3ebba7370159adb3af06ddd95679bef1
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-26 09:59:19 -0600

    added HTML content

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+	<html> lang="en">
+	<head>
+		<meta charset="utf-8" 
+		<link rel="stylesheet" href="css/sytle.css"		
+		<title>Giving it a try</title>
+	</head>
+	<body>
+		<header>
+			<div class="container">
+				<a href="#">Css demo :D </a>
+				<ul>
+					<li><a href="#"> Link 1></a> </li>
+					<li><a href="#"> Likn 2>/a> </li>
+				</ul>
+			</div>
+		</header>
+		<main>
+			<div> calss="container"</div>3
+				<p class="width-50">
+					
+				</p>
+		</main>
+	</body>
+</html>
